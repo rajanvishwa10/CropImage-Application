ed213908b12537ce37341395fe1eec23292bf4b7
rajanvishwa10 <rajanvishwa10@gmail.com>
2020-12-12 00:37:34 +0530

    final commit with share

--- app/src/main/java/com/android/cameraapplication/MainActivity.java
+++ app/src/main/java/com/android/cameraapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.cameraapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.core.graphics.BitmapCompat;
@@ -9,11 +10,19 @@ import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableWrapper;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,9 +35,12 @@ import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+
+import id.zelory.compressor.Compressor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     Uri imageUri;
     TextView textView;
     File f;
+    Bitmap bitmap, convertedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,10 +65,9 @@ public class MainActivity extends AppCompatActivity {
 //    startActivityForResult(intent,0);
     }
 
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         int dataSize = 0;
         if (requestCode == 0
                 && resultCode == Activity.RESULT_OK) {
@@ -65,34 +77,45 @@ public class MainActivity extends AppCompatActivity {
             startCrop(imageUri);
 
         }
-        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri uri = result.getUri();
-                System.out.println(uri);
-                String scheme = uri.getScheme();
-                if (scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-                    try {
-                        InputStream fileInputStream = getApplicationContext().getContentResolver().openInputStream(uri);
-                        dataSize = fileInputStream.available();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("File size in bytes" + dataSize);
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
 
-                } else if (scheme.equals(ContentResolver.SCHEME_FILE)) {
-                    String path = uri.getPath();
-                    try {
-                        f = new File(path);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("File size in bytes " + f.length());
+                    convertedImage = getResizedBitmap(bitmap, 100);
+//                    System.out.println(converetdImage);
+                } catch (Exception e) {
+                    //handle exception
                 }
+//                Bitmap bitmap = result.getBitmap();
+                System.out.println(uri);
+
+//                String scheme = uri.getScheme();
+//                if (scheme.equals(ContentResolver.SCHEME_CONTENT)) {
+//                    try {
+//                        InputStream fileInputStream = getApplicationContext().getContentResolver().openInputStream(uri);
+//                        dataSize = fileInputStream.available();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println("File size in bytes" + dataSize);
+//
+//                } else if (scheme.equals(ContentResolver.SCHEME_FILE)) {
+//                    String path = bitmap..getPath();
+//                    try {
+//                        f = new File(path);
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+                System.out.println("File size in bytes " + convertedImage.getByteCount());
+//                }
 //                int bitmapByteCount= BitmapCompat.getAllocationByteCount(bitmap);
-                textView.setText(f.length() / 1024 + " kb");
+                textView.setText(convertedImage.getByteCount() / 1024 + " kb");
                 imageView.setImageURI(uri);
+
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception exception = result.getError();
                 Toast.makeText(this, exception.toString(), Toast.LENGTH_SHORT).show();
@@ -101,10 +124,76 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
     private void startCrop(Uri uri) {
         CropImage.activity(uri).setGuidelines(CropImageView.Guidelines.ON)
                 .setMultiTouchEnabled(true)
                 .start(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                try {
+                    share();
+                } catch (NullPointerException e) {
+                    Toast.makeText(this, "No image available", Toast.LENGTH_SHORT).show();
+                }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    Bitmap bitmap1;
+
+    public void share() {
+        try {
+            Drawable drawable = imageView.getDrawable();
+            bitmap1 = ((BitmapDrawable) drawable).getBitmap();
+        } catch (Exception e) {
+            Toast.makeText(this, "No image available", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            File file = new File(this.getExternalCacheDir(), File.separator + "image.jpg");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            bitmap1.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            file.setReadable(true, false);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(MainActivity.this,
+                    BuildConfig.APPLICATION_ID + ".fileprovider", file));
+            intent.setType("image/*");
+            startActivity(Intent.createChooser(intent, "Share Image via.."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "No image available", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 }
